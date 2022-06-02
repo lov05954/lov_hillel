bc9558cd3a5b299c592fd6003eef9343b399cd4d
lov05954 <lov05954@ukr.net>
2022-06-02 21:54:42 +0300

    first commit

--- lecture08/assignment08_03.py
+++ lecture08/assignment08_03.py
@@ -10,3 +10,21 @@
             * calculate("hello world!", "*", 2) -> повертає "hello world!hello world!"
             * calculate(10, ")", 10) -> повертає None
 """
+
+#a, b, c = 2, "+", 2
+a, b, c = "hello world!", "*", 2
+#a, b, c = 10, ")", 2
+
+def calculate(opd1, opr, opd2):
+    if opr == '+':
+        return (opd1 + opd2)
+    elif opr == '-':
+        return (opd1 - opd2)
+    elif opr == '*':
+        return (opd1 * opd2)
+    elif opr == '/':
+        return (opd1 / opd2)
+    else:
+        return None
+
+print(calculate(a, b, c))
